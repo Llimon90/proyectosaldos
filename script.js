6167df587cd899ea01cc93c630addcd70cf77ed1
Luis Limón <132353147+Llimon90@users.noreply.github.com>
2024-08-08 19:21:22 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+
+
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.carousel-item');
+    const totalSlides = slides.length;
+
+    if (index >= totalSlides) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = totalSlides - 1;
+    } else {
+        currentSlide = index;
+    }
+
+    const offset = -currentSlide * 100;
+    document.querySelector('.carousel').style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+// Opcional: Cambio automático de diapositivas
+setInterval(() => {
+    nextSlide();
+}, 5000);
+
+
+//barra de navegacion
+
